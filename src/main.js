331eb1bcb10eee3dd2441e542099f7b44ecc2ce7
elias alexandre <elliasallex@gmail.com>
2021-08-23 00:11:35 -0300

    feat: toogle

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector("#header-container nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+for (const element of toggle) {
+  element.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+}
+
+// ao clicar no item, esconder o menu
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
+}
